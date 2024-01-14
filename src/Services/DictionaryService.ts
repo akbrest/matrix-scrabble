c79aa5cd94bcb09a5e38c7bb707991e8a752ea75
vs <vadim.stelikov@nortal.com>
2024-01-14 18:07:15 +0200

    add english and russian Services/DictionaryService.ts

---
+++ src/Services/DictionaryService.ts
@@ -0,0 +1,28 @@
+import RussianService from '../Services/RussianService';
+import ILanguageService from '../Services/LanguageService';
+
+class DictionaryService {
+
+  LanguageService;
+
+  public constructor(ILanguageService: ILanguageService) {
+    this.LanguageService = ILanguageService;
+  }
+
+  public async CheckWords(data){
+
+    var self = this;
+
+    await data.forEach(
+        async function(element) {
+          element.status = await self.LanguageService.GetWordStatus(element.word)
+          element.length = element.word.length
+        }
+    );
+
+    return false;
+  }
+
+}
+  
+export default DictionaryService;
---
+++ src/Services/EnglishService.ts
@@ -0,0 +1,57 @@
+import ILanguageService from '../Services/LanguageService';
+
+class EnglishService implements ILanguageService{
+
+    async getDefinition(word) {
+      try {
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const data = await response.json();
+  
+        // Assuming the API returns an array of definitions
+        const firstDefinition = data;
+  
+        return firstDefinition || 'No definition';
+      } catch (error) {
+        console.error('Error fetching definition:', error);
+        throw new Error('Error fetching definition');
+      }
+    }
+
+    public async GetWordStatus(word) {
+      try {
+      
+        var data = await this.getDefinition(word);
+        
+        if(data == null) return "err";
+        if(data === 'No definition') return "err";
+        if(data[0] == null) return "err";
+        if(data[0].meanings == null) return "err" ;
+
+        var value = false
+        
+        data.map((element) => {
+
+          element.meanings.map((elem) => {
+            if(elem.partOfSpeech === 'noun'){
+              value = true;
+            }
+
+            return 1;
+          })
+
+          return 1;
+        })
+        
+        if (value)
+          return "ok";
+        else
+          return "err";
+
+      } catch (error) {
+        console.error('Error fetching definition:', error);
+        throw new Error('Error fetching definition');
+      }
+    }
+  }
+  
+  export default EnglishService;
