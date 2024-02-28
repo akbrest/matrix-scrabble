05962dbbb3f916ebc98248133c0be1158931bc2f
Aliaksandr Kandratsiuk <ak.brest@gmail.com>
2024-02-28 22:46:19 +0200

    Code review changes

--- matrixscrabble.client/src/redux/games/reducer.ts
+++ matrixscrabble.client/src/redux/games/reducer.ts
@@ -6,6 +6,28 @@ const gamesReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case actionTypes.CREATE_GAME:
       return [...state, action.payload];
+    // case GAME_ACTIONS.CREATE_GAME_REQUEST: {
+    //   return {
+    //     ...state,
+    //     id: actions.payload,
+    //     loading: true,
+    //   };
+    // }
+    // case GAME_ACTIONS.CREATE_GAME_SUCCESS: {
+    //   return {
+    //     ...state,
+    //     id: actions.payload.id,
+    //     loading: false,
+    //   };
+    // }
+    // case GAME_ACTIONS.CREATE_GAME_FAILURE: {
+    //   return {
+    //     ...state,
+    //     data: null,
+    //     loading: false,
+    //     error: actions.payload,
+    //   };
+    // }
     default:
       return state;
   }
