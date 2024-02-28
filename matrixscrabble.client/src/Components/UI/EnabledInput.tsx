074d043bb1c26310ee2532ce30b8fb3bb61407d5
Aliaksandr Kandratsiuk <ak.brest@gmail.com>
2024-02-29 00:06:18 +0200

    changes after rebase

--- matrixscrabble.client/src/Components/UI/EnabledInput.tsx
+++ matrixscrabble.client/src/Components/UI/EnabledInput.tsx
@@ -1,62 +1,62 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 
-interface OneLetterEnabledInputProps {
-    language: string;
-    UpdateField: (x: number, y: number, type: string, value: string) => void;
-    row: number;
-    type:string
-}
+//interface OneLetterEnabledInputProps {
+//    language: string;
+//    UpdateField: (x: number, y: number, type: string, value: string) => void;
+//    row: number;
+//    type:string
+//}
 
-const AllowedLetters = {
-    'en': "AaBbCcDdEeFfGgHIihJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",
-    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
-}
+//const AllowedLetters = {
+//    'en': "AaBbCcDdEeFfGgHIihJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",
+//    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+//}
 
-const AllLetters = ['а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'Ъ', 'ъ', 'Ы', 'ы', 'Ь', 'ь',
-    'Э',
-    'э',
-    'Ю',
-    'ю',
-    'Я',
-    'я'];
+//const AllLetters = ['а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'Ъ', 'ъ', 'Ы', 'ы', 'Ь', 'ь',
+//    'Э',
+//    'э',
+//    'Ю',
+//    'ю',
+//    'Я',
+//    'я'];
 
 
-const EnabledInput: React.FC<OneLetterEnabledInputProps> = ({ row, type, language, UpdateField }) => {
-    const [lang, setLanguage] = useState(language);
-    const [value, setValue] = useState("");
-    const [width, setWidth] = useState("");
-    const [rowValue, setRowValue] = useState(row);
-    const [typeX, setType] = useState(type);
-    let textInput = React.createRef();
+//const EnabledInput: React.FC<OneLetterEnabledInputProps> = ({ row, type, language, UpdateField }) => {
+//    const [lang, setLanguage] = useState(language);
+//    const [value, setValue] = useState("");
+//    const [width, setWidth] = useState("");
+//    const [rowValue, setRowValue] = useState(row);
+//    const [typeX, setType] = useState(type);
+//    let textInput = React.createRef();
 
-    function checkValidity(e: any) {
-        var elem = e.key;
-        var allowedChars = AllowedLetters[lang];
-        if (allowedChars.indexOf(elem) >= 0) {
-            setValue(elem);
-        } else {
-            if (elem == "Delete" || elem == "Backspace") {
+//    function checkValidity(e: any) {
+//        var elem = e.key;
+//        var allowedChars = AllowedLetters[lang];
+//        if (allowedChars.indexOf(elem) >= 0) {
+//            setValue(elem);
+//        } else {
+//            if (elem == "Delete" || elem == "Backspace") {
 
-        return true;
-    }
+//        return true;
+//    }
 
-        return true;
-    }
+//        return true;
+//    }
 
-    function onChange(e: any) {
-        var length = e.target.value.length;
-        var w = length * 20;
-        setWidth(111);
-    }
+//    function onChange(e: any) {
+//        var length = e.target.value.length;
+//        var w = length * 20;
+//        setWidth(111);
+//    }
 
-    return <div style={{ width: '100px' }} className={'ddd'}>
-        <input ref={textInput} 
-            onChange={() => UpdateField(rowValue, 0, typeX, textInput.current.value)}
-            onKeyDown={(e) => checkValidity(e)}
-            className="simpleInput"
-            type="text"
-        />
-    </div>
-};
+//    return <div style={{ width: '100px' }} className={'ddd'}>
+//        <input ref={textInput} 
+//            onChange={() => UpdateField(rowValue, 0, typeX, textInput.current.value)}
+//            onKeyDown={(e) => checkValidity(e)}
+//            className="simpleInput"
+//            type="text"
+//        />
+//    </div>
+//};
 
-export default EnabledInput;
+//export default EnabledInput;
--- matrixscrabble.client/src/Components/UI/OneLetterEnabledInput.tsx
+++ matrixscrabble.client/src/Components/UI/OneLetterEnabledInput.tsx
@@ -1,51 +1,51 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 
-interface OneLetterDisabledInputProps {
-    language: string;
-    UpdateField: (x: number, y: number, type: string, value: string) => void;
-    x: number;
-    y: number;
-}
+//interface OneLetterDisabledInputProps {
+//    language: string;
+//    UpdateField: (x: number, y: number, type: string, value: string) => void;
+//    x: number;
+//    y: number;
+//}
 
-const AllowedLetters = {
-    'en': "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwVvXxYYZz",
-    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
-}
+//const AllowedLetters = {
+//    'en': "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuWwVvXxYYZz",
+//    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+//}
 
-const OneLetterEnabledInput: React.FC<OneLetterDisabledInputProps> = ({x, y,  language, UpdateField }) => {
-    const [lang, setLanguage] = useState(language);
-    const [value, setValue] = useState("");
-    const [xx, setX] = useState(x);
-    const [yy, setY] = useState(y);
+//const OneLetterEnabledInput: React.FC<OneLetterDisabledInputProps> = ({x, y,  language, UpdateField }) => {
+//    const [lang, setLanguage] = useState(language);
+//    const [value, setValue] = useState("");
+//    const [xx, setX] = useState(x);
+//    const [yy, setY] = useState(y);
 
-    function checkValidity(e: any) {
-        var elem = e.key;
-        var allowedChars = AllowedLetters[lang];
+//    function checkValidity(e: any) {
+//        var elem = e.key;
+//        var allowedChars = AllowedLetters[lang];
         
-        if (allowedChars.indexOf(elem) >= 0) {
-            setValue(elem);
-        } else {
-            if (elem == "Delete" || elem == "Backspace") {
+//        if (allowedChars.indexOf(elem) >= 0) {
+//            setValue(elem);
+//        } else {
+//            if (elem == "Delete" || elem == "Backspace") {
 
-            } else {
-                e.preventDefault();
-                return false;
-            }
-        }
+//            } else {
+//                e.preventDefault();
+//                return false;
+//            }
+//        }
 
-        return true;
-    }
+//        return true;
+//    }
 
-    return <div className="one-symbol-enabled-block">
-        <input
-            onChange={() => UpdateField(xx, yy, "main", value)}
-            onKeyDown={(e) => checkValidity(e)}
-            maxLength={1}
-            className="simpleInput"
-            type="text"
-            size={1}
-        />
-    </div>
+//    return <div className="one-symbol-enabled-block">
+//        <input
+//            onChange={() => UpdateField(xx, yy, "main", value)}
+//            onKeyDown={(e) => checkValidity(e)}
+//            maxLength={1}
+//            className="simpleInput"
+//            type="text"
+//            size={1}
+//        />
+//    </div>
 
-};
-export default OneLetterEnabledInput;
+//};
+//export default OneLetterEnabledInput;
