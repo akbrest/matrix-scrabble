1f67bc8a25669064303a54e48ded084d2e6c14e3
vs <vadim.stelikov@nortal.com>
2024-02-29 11:36:23 +0200

    fix linting for components

--- matrixscrabble.client/src/Components/UI/EnabledInput.tsx
+++ matrixscrabble.client/src/Components/UI/EnabledInput.tsx
@@ -1,62 +1,49 @@
-//import React, { useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
 
-//interface OneLetterEnabledInputProps {
-//    language: string;
-//    UpdateField: (x: number, y: number, type: string, value: string) => void;
-//    row: number;
-//    type:string
-//}
+interface OneLetterEnabledInputProps {
+    language: string;
+    UpdateField: (x: number, y: number, type: string, value: string) => void;
+    row: number;
+    type: string
+}
 
-//const AllowedLetters = {
-//    'en': "AaBbCcDdEeFfGgHIihJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",
-//    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
-//}
+const AllowedLetters = {
+    'en': "AaBbCcDdEeFfGgHIihJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",
+    'ru': "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
+}
 
-//const AllLetters = ['а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'Ъ', 'ъ', 'Ы', 'ы', 'Ь', 'ь',
-//    'Э',
-//    'э',
-//    'Ю',
-//    'ю',
-//    'Я',
-//    'я'];
+const EnabledInput: React.FC<OneLetterEnabledInputProps> = ({ row, type, language, UpdateField }) => {
+    const [lang] = useState(language);
+    const [rowValue] = useState(row);
+    const [typeX] = useState(type);
+    const textInput = React.createRef();
 
 
-//const EnabledInput: React.FC<OneLetterEnabledInputProps> = ({ row, type, language, UpdateField }) => {
-//    const [lang, setLanguage] = useState(language);
-//    const [value, setValue] = useState("");
-//    const [width, setWidth] = useState("");
-//    const [rowValue, setRowValue] = useState(row);
-//    const [typeX, setType] = useState(type);
-//    let textInput = React.createRef();
+    function CheckValidity(e: React.KeyboardEvent<HTMLInputElement>): void {
+        const elem = e.key;
+        const allowedChars = AllowedLetters[lang];
+        if (allowedChars.indexOf(elem) < 0) 
+        {
+            if (elem == "Delete" || elem == "Backspace") {
+                e.preventDefault();
+                return;
+            }
+        }
 
-//    function checkValidity(e: any) {
-//        var elem = e.key;
-//        var allowedChars = AllowedLetters[lang];
-//        if (allowedChars.indexOf(elem) >= 0) {
-//            setValue(elem);
-//        } else {
-//            if (elem == "Delete" || elem == "Backspace") {
+        return;
+    }
 
-//        return true;
-//    }
 
-//        return true;
-//    }
+    return <div style={{ width: '100px' }}>
+        <input ref={textInput}
+            onChange={() => UpdateField(rowValue, 0, typeX, textInput.current.value)}
+            onKeyDown={(e) => CheckValidity(e)}
+            className="simpleInput"
+            type="text"
+        />
+    </div>
+}
 
-//    function onChange(e: any) {
-//        var length = e.target.value.length;
-//        var w = length * 20;
-//        setWidth(111);
-//    }
 
-//    return <div style={{ width: '100px' }} className={'ddd'}>
-//        <input ref={textInput} 
-//            onChange={() => UpdateField(rowValue, 0, typeX, textInput.current.value)}
-//            onKeyDown={(e) => checkValidity(e)}
-//            className="simpleInput"
-//            type="text"
-//        />
-//    </div>
-//};
-
-//export default EnabledInput;
+export default EnabledInput;
--- matrixscrabble.client/src/Components/UI/OneLetterDisabledInput.tsx
+++ matrixscrabble.client/src/Components/UI/OneLetterDisabledInput.tsx
@@ -5,8 +5,8 @@ interface OneLetterDisabledInputProps {
 const OneLetterDisabledInput: React.FC<OneLetterDisabledInputProps> = ({ letter }) => {
     return <div className="one-symbol-enabled-block">
         <div className="word-block">{letter}</div>
-        </div>
-    
+    </div>
+
 };
 
 export default OneLetterDisabledInput;
