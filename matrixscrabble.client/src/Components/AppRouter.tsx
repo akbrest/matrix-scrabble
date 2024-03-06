8bbcc40cc7af310ae2dcf423f1d409cd698f7700
vs <vadim.stelikov@nortal.com>
2024-03-06 19:58:43 +0200

    fix

--- matrixscrabble.client/src/Components/AppRouter.tsx
+++ matrixscrabble.client/src/Components/AppRouter.tsx
@@ -3,20 +3,19 @@ import MainLayout from './MainLayout';
 import Home from './Home';
 import GamesPage from './GamesPage/GamesPage';
 import SingleGame from './GamesPage/SingleGame';
-import Menu from './Menu';
 
 const AppRouter = () => {
     return (
         <div>
-        <Routes>
-            <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="games" element={<GamesPage />} />
-            <Route path="games/:id" element={<SingleGame />} />
-            </Route>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="games" element={<GamesPage />} />
+                    <Route path="games/:id" element={<SingleGame />} />
+                </Route>
             </Routes>
         </div>
-  );
+    );
 };
 
 export default AppRouter;
